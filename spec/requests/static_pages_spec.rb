9f5b520b8d42dcb745aea1b8ce39b3b2535ea7d8
Richard Noble <rgn15996@gmail.com>
2013-01-05 12:29:53 +0000

    Complete commit of static pages and tests, haml etc

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,8 @@ describe "Static Pages" do
     end
     it "should have the correct title" do
       visit '/static_pages/home'
-      page.should have_selector('title', text: "Account CTO | Home")
+      # puts page.html
+      page.should have_title("Account CTO | Home")
   	end
   end
   describe "Help Page" do
@@ -16,11 +17,21 @@ describe "Static Pages" do
       visit '/static_pages/help'
       page.should have_content('Help for the ACTO Application')
     end
+    it "should have the correct title" do
+      visit '/static_pages/help'
+      # puts page.html
+      page.should have_title("Account CTO | Help")
+  	end
   end
   describe "About Page" do
     it "should have the content 'About the ACTO Application'" do
       visit '/static_pages/about'
       page.should have_content('About the ACTO Application')
     end
+    it "should have the correct title" do
+      visit '/static_pages/about'
+      # puts page.html
+      page.should have_title("Account CTO | About")
+  	end
   end
 end
