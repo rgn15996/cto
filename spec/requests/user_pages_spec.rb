8f8415ffe739cee3f6c0eced1f8e227ca06e270b
Richard Noble <rgn15996@gmail.com>
2013-01-06 00:13:13 +0000

    Finish sign-in/out code and tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,6 +54,8 @@ describe "User pages" do
 
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+        it { should_not have_link('Sign in') }
       end
     end
   end
