e8badab5300145084bb3df5c7770d3d0d19ee577
Richard Noble <rgn15996@gmail.com>
2013-02-06 07:55:24 +0000

    Add small ratings stars to innovation partial
    This is basic as it just takes the rounded average score

--- app/helpers/innovation_ideas_helper.rb
+++ app/helpers/innovation_ideas_helper.rb
@@ -2,11 +2,14 @@ module InnovationIdeasHelper
   
   def rating_stars(score)
     capture_haml do
-      haml_concat(text='= "Hello World"')
-      haml_tag :span, {class: "rating", data: { score: score}} do 
+      haml_tag :span, class: "rating" do 
         1.upto(5) do |element|
-          haml_tag :span, class: "star", id: "score-#{element}" do
-            haml_tag :i, class: "icon-star"
+          haml_tag :span, class: "small-star" do
+            if element < score then
+              haml_tag :i, {class: "icon-star", style: "color: red"}
+            else
+              haml_tag :i, class: "icon-star"
+            end
           end
         end
       end
